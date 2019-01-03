494d2e882698174b627bc3452213d2be88aa9823
huangkai <18778335525@163.com>
2019-01-03 22:06:02 +0800

    修复错误

--- src/main/java/com/huangkai/car/Car.java
+++ src/main/java/com/huangkai/car/Car.java
@@ -20,20 +20,5 @@ public class Car {
     private Long id;
     private @NonNull String name;
 
-    void setName(String name) {
-        this.name = name;
-    }
-
-    String getName() {
-        return name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
 }
